6e95e4007e6ab94c299bae6a0b5dcbb64724d776
kamel740 <kamelelsayed740@gmail.com>
2025-10-11 20:59:56 +0300

    done

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,18 +1,35 @@
 const quoteDisplay = document.getElementById('quoteDisplay');
 const newQuoteBtn = document.getElementById('newQuote');
 
-const quotes = [
-  { text: "The only way to do great work is to love what you do.", category: "Inspiration" },
-  { text: "Success is not final, failure is not fatal: it is the courage to continue that counts.", category: "Motivation" },
-  { text: "In the middle of every difficulty lies opportunity.", category: "Wisdom" },
-  { text: "Don’t watch the clock; do what it does. Keep going.", category: "Perseverance" },
-  { text: "Happiness is not something ready-made. It comes from your own actions.", category: "Happiness" },
-  { text: "The best way to predict your future is to create it.", category: "Self-Improvement" },
-  { text: "You miss 100% of the shots you don’t take.", category: "Courage" },
-  { text: "Do what you can, with what you have, where you are.", category: "Mindset" },
-  { text: "Believe you can and you're halfway there.", category: "Confidence" },
-  { text: "Dream big. Start small. Act now.", category: "Action" }
+let quotes = [];
+const defaultQuotes = [
+    { text: "The only way to do great work is to love what you do.", category: "Inspiration" },
+    { text: "Success is not final, failure is not fatal: it is the courage to continue that counts.", category: "Motivation" },
+    { text: "In the middle of every difficulty lies opportunity.", category: "Wisdom" },
+    { text: "Don’t watch the clock; do what it does. Keep going.", category: "Perseverance" },
+    { text: "Happiness is not something ready-made. It comes from your own actions.", category: "Happiness" },
+    { text: "The best way to predict your future is to create it.", category: "Self-Improvement" },
+    { text: "You miss 100% of the shots you don’t take.", category: "Courage" },
+    { text: "Do what you can, with what you have, where you are.", category: "Mindset" },
+    { text: "Believe you can and you're halfway there.", category: "Confidence" },
+    { text: "Dream big. Start small. Act now.", category: "Action" }
 ];
+
+function loadQuotes(){
+    const loadedQuotes = localStorage.getItem('savedQuotesInMemory');
+    if (loadedQuotes){
+       quotes = JSON.parse(loadedQuotes)
+    }
+    else{
+        quotes = defaultQuotes;
+    }
+}
+loadQuotes();
+
+function saveQuotes(){
+   const stringfyedQutoes = JSON.stringify(quotes);
+   localStorage.setItem('savedQuotesInMemory',stringfyedQutoes);
+}
 
 function showRandomQuote(){
     const randomIndex = Math.floor(Math.random() * quotes.length);
@@ -20,9 +37,25 @@ function showRandomQuote(){
     quoteDisplay.innerHTML = `<p>"${randomQuote.text}"</p>
     <p><em>${randomQuote.category}</em></p>`;
     console.log(randomIndex)
+
+    const lastQuoteStringifyed = JSON.stringify(randomQuote);
+    sessionStorage.setItem('lastQuote',lastQuoteStringifyed);
 }
 
-showRandomQuote();
+function loadLastQuote(){
+    const sessoinData = sessionStorage.getItem('lastQuote');
+    if(sessoinData){
+        const LQuote = JSON.parse(sessoinData);
+        quoteDisplay.innerHTML = `<p>"${LQuote.text}"</p>
+                                 <p><em>${LQuote.category}</em></p>`;
+    }
+    else{
+        showRandomQuote();
+    }
+}
+loadLastQuote();
+
+// showRandomQuote();
 newQuoteBtn.addEventListener('click', showRandomQuote);
 
 function createAddQuoteForm(){
@@ -57,13 +90,14 @@ function addQuote (){
     const newQuoteCategory = document.getElementById('newQuoteCategory');
 
 
-    if(newQuoteCategory && newQuoteText){
+    if(newQuoteCategory.value && newQuoteText.value){
         const newQuote = {
             text: newQuoteText.value,
             category: newQuoteCategory.value
         }
         quotes.push(newQuote)
         alert('New quote added')
+        saveQuotes();
     }
     else{
         alert('Please fill both fileds')
@@ -72,3 +106,36 @@ function addQuote (){
     document.getElementById('newQuoteText').value = '';
     document.getElementById('newQuoteCategory').value = '';
 }
+
+const exportBtn = document.createElement('button');
+exportBtn.textContent = 'Export';
+document.body.appendChild(exportBtn);
+
+function jsonExprot(){
+    const stringifyQuotes = JSON.stringify(quotes);
+    const myblob = new Blob([stringifyQuotes],{type: 'application/json'});
+    const urlLink =URL.createObjectURL(myblob);
+    const htmlLink = document.createElement('a');
+    htmlLink.href = urlLink;
+    htmlLink.download = 'quotes.json';
+    htmlLink.click();
+}
+exportBtn.addEventListener('click', jsonExprot);
+
+const importFile = document.createElement('input');
+importFile.type = 'file';
+importFile.accept = '.json';
+importFile.id = 'importFile';
+document.body.appendChild(importFile);
+
+  function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
+  importFile.addEventListener('change',importFromJsonFile);
